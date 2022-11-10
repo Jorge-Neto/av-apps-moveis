d85aa544c99273a86cb9256d52bafa2308c124c2
Jorge-Neto <neto_tunas@hotmail.com>
2022-11-09 23:12:23 -0300

    Added log-in/out features

--- lib/pages/Login/login_page.dart
+++ lib/pages/Login/login_page.dart
@@ -2,12 +2,20 @@ import 'package:avaliacao/core/app_colors.dart';
 import 'package:avaliacao/core/app_images.dart';
 import 'package:avaliacao/core/app_text.dart';
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
+  final controllerUsername = TextEditingController();
+  final controllerPassword = TextEditingController();
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: AppColors.secondary,
       body: Padding(
@@ -23,7 +31,29 @@ class LoginPage extends StatelessWidget {
               'Usuário',
               style: AppTextStyles.textWhite,
             ),
-            TextFormField(),
+            TextField(
+              style: TextStyle(color: Colors.white),
+              controller: controllerUsername,
+              keyboardType: TextInputType.text,
+              textCapitalization: TextCapitalization.none,
+              autocorrect: false,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.yellow)),
+                  labelText: 'Username'),
+            ),
+            TextField(
+              style: TextStyle(color: Colors.white),
+              controller: controllerPassword,
+              obscureText: true,
+              keyboardType: TextInputType.text,
+              textCapitalization: TextCapitalization.none,
+              autocorrect: false,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.yellow)),
+                  labelText: 'Password'),
+            ),
             const SizedBox(
               height: 20,
             ),
@@ -32,7 +62,7 @@ class LoginPage extends StatelessWidget {
               height: 39,
               child: ElevatedButton(
                 onPressed: () {
-                  Navigator.of(context).pushReplacementNamed("/home");
+                  doUserLogin();
                 },
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all(AppColors.primary),
@@ -44,5 +74,64 @@ class LoginPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void showSuccess(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Success!"),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void showError(String errorMessage) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Error!"),
+          content: Text(errorMessage),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void doUserLogin() async {
+    final username = controllerUsername.text.trim();
+    final password = controllerPassword.text.trim();
+    if(username.length < 1 || password.length < 1) {
+      showError("Todos os campos devem ser preenchidos");
+    }
+
+    final user = ParseUser(username, password, null);
+
+    var response = await user.login();
+
+    if (response.success) {
+      showSuccess("User was successfully login!");
+      Navigator.of(context).pushReplacementNamed("/home");
+    } else {
+      showError(response.error!.message);
+    }
   }
 }
